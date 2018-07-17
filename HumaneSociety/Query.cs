241517ef25b959783d4fd8ac1f4577d1e10aa3ee
EleMint <dylanschmidt0216@gmail.com>
2018-07-17 14:30:48 -0500

    Starter Code

---
+++ HumaneSociety/Query.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class Query
+    {
+        
+        public static void RunEmployeeQueries()
+        {
+
+        }
+        public static void GetRoom()
+        {
+
+        }
+        public static void GetPendingAdoptions()
+        {
+
+        }
+        public static void UpdateAdoption()
+        {
+
+        }
+    }
+}
