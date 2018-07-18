08a051c63ed044c1370b245cac89bd66f5e4e22f
EleMint <dylanschmidt0216@gmail.com>
2018-07-18 10:05:58 -0500

    Added a unit test for GetPendingAdoptions

---
+++ HumaneSocietyTests/GetPendingAdoptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HumaneSociety;
+
+namespace HumaneSocietyTests
+{
+    [TestClass]
+    public class GetPendingAdoptions
+    {
+        [TestMethod]
+        public void TwoIndex_GetPendingAdoptions()
+        {
+            var adoptionList = Query.GetPendingAdoptions();
+            // Assert
+            foreach(Adoption a in adoptionList)
+            {
+                //Assert.IsInstanceOfType();
+            }
+        }
+    }
+}
---
+++ HumaneSocietyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HumaneSocietyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HumaneSocietyTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44f47553-eacb-4780-a623-e84296ba794f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
